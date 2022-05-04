65ae6eb8e09f0a72da51aa468e06a871158db0d0
yuhangbin <yuhangbinccc@gmail.com>
2022-05-05 00:22:37 +0800

    feat: ApplicationContext

---
+++ src/main/java/com/yu/spring/context/AbstractApplicationContext.java
@@ -0,0 +1,8 @@
+package com.yu.spring.context;
+
+/**
+ * @author yuhangbin
+ * @date 2022/5/4
+ **/
+public abstract class AbstractApplicationContext {
+}
---
+++ src/main/java/com/yu/spring/context/ApplicationContext.java
@@ -0,0 +1,12 @@
+package com.yu.spring.context;
+
+import com.yu.spring.beans.factory.BeanFactory;
+
+/**
+ * @author yuhangbin
+ * @date 2022/5/3
+ **/
+public interface ApplicationContext extends BeanFactory {
+
+
+}
