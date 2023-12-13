62efc5938d903a0467d0a712eb562a673233e26d
yuhangbin <yuhangbinccc@gmail.com>
2023-12-13 23:59:39 +0800

    feat: Resource

--- src/main/java/com/yu/spring/io/Resource.java
+++ src/main/java/com/yu/spring/io/Resource.java
@@ -1,5 +1,14 @@
-package com.yu.spring.io;/**
+package com.yu.spring.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
  * @author yuhangbin
  * @date 2022/5/8
- **/public interface Resource {
+ **/
+public interface Resource {
+
+	InputStream getInputStream() throws IOException;
+
 }
